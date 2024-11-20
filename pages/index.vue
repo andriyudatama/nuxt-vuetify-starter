3bc9ac42591d18fb1285362e76be0e3adf2f0fe7
Andri Yudatama <andriecool@gmail.com>
2024-11-21 05:27:44 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-responsive 
+    class="align-center fill-height mx-auto"
+    max-width="900">
+    <div class="text-center">
+        <div class="text-body-2 font-weight-light">Welcome to</div>
+        <h1 class="text-h2 font-weight-bold">Nuxt + Vuetify Starter</h1>
+        <div class="text-body-2 font-weight-light mt-2">Created by Andri Yudatama</div>
+      </div>
+    <v-row>
+      <v-col cols="4">
+      </v-col>
+    </v-row>
+  </v-responsive>
+</template>
